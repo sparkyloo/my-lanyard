8eae84a23c0c552603fc80763e0492faa52d73e5
Aman Kaur <amanpreet_k32@yahoo.com>
2023-02-21 21:49:03 -0800

    begin my database

---
+++ backend/db/models/icon.js
@@ -0,0 +1,24 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Icon extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Icon.init({
+    name: DataTypes.STRING,
+    imageUrl: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Icon',
+  });
+  return Icon;
+};
