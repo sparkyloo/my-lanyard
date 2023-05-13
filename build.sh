8761bda829ea755c44e5a1a98db3bd8d7debb558
Aman Kaur <amanpreet_k32@yahoo.com>
2023-05-13 00:37:43 -0700

    fix build script

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 npm install
 npm run build
 npm --prefix backend run sequelize -- db:seed:undo:all
-npm --prefix backend run sequelize -- db:migrate:undo:all
-npm --prefix backend run sequelize -- db:migrate
+node backend/psql-setup-script.js
+node backend/sync-models.js
 npm --prefix backend run sequelize -- db:seed:all
