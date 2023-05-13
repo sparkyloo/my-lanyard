add801ee594869c1357779a879efb0b654247b4d
Aman Kaur <amanpreet_k32@yahoo.com>
2023-05-13 00:57:53 -0700

    maybe now it works

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 npm install
 npm run build
 npm --prefix backend run sequelize -- db:seed:undo:all
-node backend/psql-setup-script.js
 node backend/sync-models.js
 npm --prefix backend run sequelize -- db:seed:all
