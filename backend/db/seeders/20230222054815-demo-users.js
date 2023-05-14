f3e9ec4c0014098795001173d265de7993e70c67
Aman Kaur <amanpreet_k32@yahoo.com>
2023-05-14 12:19:24 -0700

    add more demo data

--- backend/db/seeders/20230222054815-demo-users.js
+++ backend/db/seeders/20230222054815-demo-users.js
@@ -3,7 +3,7 @@
 const fs = require("fs/promises");
 const path = require("path");
 const bcrypt = require("bcryptjs");
-const { User, Icon, Tag, Tagging } = require("../models");
+const { User, Icon, Tag, Tagging, Card, Lanyard } = require("../models");
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
@@ -102,6 +102,58 @@ module.exports = {
       }
     );
 
+    const upsetLanyard = await Lanyard.create({
+      authorId: admin.id,
+      name: "Upset",
+      description: "Unpleasant emotions",
+    });
+
+    await Tagging.create({
+      authorId: admin.id,
+      tagId: emotionTag.id,
+      lanyardId: upsetLanyard.id,
+    });
+
+    const upsetCards = await Card.bulkCreate(
+      emotionIcons
+        .filter((icon) => {
+          switch (icon.name) {
+            case "angry":
+            case "mad":
+            case "sad": {
+              return true;
+            }
+            default: {
+              return false;
+            }
+          }
+        })
+        .map((icon) => {
+          return {
+            authorId: admin.id,
+            iconId: icon.id,
+            lanyardId: upsetLanyard.id,
+            text: icon.name,
+          };
+        }),
+      {
+        validate: true,
+      }
+    );
+
+    await Tagging.bulkCreate(
+      upsetCards.map((upsetCard) => {
+        return {
+          authorId: admin.id,
+          tagId: emotionTag.id,
+          cardId: upsetCard.id,
+        };
+      }),
+      {
+        validate: true,
+      }
+    );
+
     const foodIconImages = await fs.readdir(
       path.join(
         __dirname,
@@ -112,6 +164,16 @@ module.exports = {
     const foodTag = await Tag.create({
       authorId: admin.id,
       name: "food",
+    });
+
+    const fruitTag = await Tag.create({
+      authorId: admin.id,
+      name: "fruit",
+    });
+
+    const drinkTag = await Tag.create({
+      authorId: admin.id,
+      name: "drink",
     });
 
     const foodIcons = await Icon.bulkCreate(
@@ -127,14 +189,163 @@ module.exports = {
       }
     );
 
+    const fruitIcons = foodIcons.filter((icon) => {
+      switch (icon.name) {
+        case "apple":
+        case "banana":
+        case "orange":
+        case "strawberry":
+        case "watermelon": {
+          return true;
+        }
+        default: {
+          return false;
+        }
+      }
+    });
+
+    const drinkIcons = foodIcons.filter((icon) => {
+      switch (icon.name) {
+        case "apple juice":
+        case "bottled water":
+        case "orange juice":
+        case "milk":
+        case "water": {
+          return true;
+        }
+        default: {
+          return false;
+        }
+      }
+    });
+
     await Tagging.bulkCreate(
-      foodIcons.map((foodIcon) => {
-        return {
+      [
+        ...foodIcons.map((foodIcon) => {
+          return {
+            authorId: admin.id,
+            tagId: foodTag.id,
+            iconId: foodIcon.id,
+          };
+        }),
+        ...fruitIcons.map((fruitIcon) => {
+          return {
+            authorId: admin.id,
+            tagId: fruitTag.id,
+            iconId: fruitIcon.id,
+          };
+        }),
+        ...drinkIcons.map((drinkIcon) => {
+          return {
+            authorId: admin.id,
+            tagId: drinkTag.id,
+            iconId: drinkIcon.id,
+          };
+        }),
+      ],
+      {
+        validate: true,
+      }
+    );
+
+    const fruitLanyard = await Lanyard.create({
+      authorId: admin.id,
+      name: "Fruit",
+      description: "Healthy snacks",
+    });
+
+    const drinkLanyard = await Lanyard.create({
+      authorId: admin.id,
+      name: "Drink",
+    });
+
+    await Tagging.bulkCreate(
+      [
+        {
           authorId: admin.id,
           tagId: foodTag.id,
-          iconId: foodIcon.id,
+          lanyardId: fruitLanyard.id,
+        },
+        {
+          authorId: admin.id,
+          tagId: foodTag.id,
+          lanyardId: drinkLanyard.id,
+        },
+        {
+          authorId: admin.id,
+          tagId: fruitTag.id,
+          lanyardId: fruitLanyard.id,
+        },
+        {
+          authorId: admin.id,
+          tagId: drinkTag.id,
+          lanyardId: drinkLanyard.id,
+        },
+      ],
+      {
+        validate: true,
+      }
+    );
+
+    const fruitCards = await Card.bulkCreate(
+      fruitIcons.map((icon) => {
+        return {
+          authorId: admin.id,
+          iconId: icon.id,
+          lanyardId: fruitLanyard.id,
+          text: icon.name,
         };
       }),
+      {
+        validate: true,
+      }
+    );
+
+    const drinkCards = await Card.bulkCreate(
+      drinkIcons.map((icon) => {
+        return {
+          authorId: admin.id,
+          iconId: icon.id,
+          lanyardId: drinkLanyard.id,
+          text: icon.name,
+        };
+      }),
+      {
+        validate: true,
+      }
+    );
+
+    await Tagging.bulkCreate(
+      [
+        ...fruitCards.flatMap((fruitCard) => {
+          return [
+            {
+              authorId: admin.id,
+              tagId: foodTag.id,
+              cardId: fruitCard.id,
+            },
+            {
+              authorId: admin.id,
+              tagId: fruitTag.id,
+              cardId: fruitCard.id,
+            },
+          ];
+        }),
+        ...drinkCards.flatMap((drinkCard) => {
+          return [
+            {
+              authorId: admin.id,
+              tagId: foodTag.id,
+              cardId: drinkCard.id,
+            },
+            {
+              authorId: admin.id,
+              tagId: drinkTag.id,
+              cardId: drinkCard.id,
+            },
+          ];
+        }),
+      ],
       {
         validate: true,
       }
